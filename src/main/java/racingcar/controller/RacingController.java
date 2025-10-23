17cf74e1dc5b49d26b639905016bcc4d30319fa7
gudwns1812 <gudwns1812@naver.com>
2025-10-23 18:29:40 +0900

    feat: 사용자에게 시도할 횟수를 받아서 검증하는 기능 추가

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,16 +1,18 @@
 package racingcar.controller;
 
 import racingcar.domain.Participants;
+import racingcar.domain.RacingTrack;
 import racingcar.view.InputView;
 
 public class RacingController {
 
     public RacingController() {
-        ;
     }
 
     public void run() {
         String nameString = InputView.readName();
         Participants participants = Participants.participate(nameString);
+        RacingTrack.participateBy(participants);
+        String move = InputView.readName();
     }
 }
--- src/main/java/racingcar/util/Validator.java
+++ src/main/java/racingcar/util/Validator.java
@@ -9,4 +9,18 @@ public final class Validator {
             throw new IllegalArgumentException("이름의 길이가 5보다 큽니다");
         }
     }
+
+    public static void validateNumber(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 숫자 형식입니다.");
+        }
+    }
+
+    public static void validateNumberNegative(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+        }
+    }
 }
