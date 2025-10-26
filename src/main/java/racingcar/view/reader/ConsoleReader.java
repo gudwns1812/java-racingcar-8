c16189502a4bf75d3c33b97bac58ca0aa9cd77c6
gudwns1812 <gudwns1812@naver.com>
2025-10-26 13:17:02 +0900

    refactor(InputView): InputView를 인터페이스로 변경

--- src/main/java/racingcar/view/reader/ConsoleReader.java
+++ src/main/java/racingcar/view/reader/ConsoleReader.java
@@ -2,7 +2,10 @@ package racingcar.view.reader;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class ConsoleReader extends InputView {
+public class ConsoleReader implements InputView {
+    private static final String INPUT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_MOVE_COUNT = "시도할 횟수는 몇 회인가요?";
+
     @Override
     public String readName() {
         System.out.println(INPUT_NAME);
--- src/main/java/racingcar/view/reader/InputView.java
+++ src/main/java/racingcar/view/reader/InputView.java
@@ -1,11 +1,6 @@
 package racingcar.view.reader;
 
-public abstract class InputView {
-
-    protected static final String INPUT_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    protected static final String INPUT_MOVE_COUNT = "시도할 횟수는 몇 회인가요?";
-
-    public abstract String readName();
-
-    public abstract String readMoveCount();
+public interface InputView {
+    String readName();
+    String readMoveCount();
 }
